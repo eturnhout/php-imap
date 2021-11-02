9083e8c0b5a126762662cb560abb4511072146a3
eturnhout <eelketurnhout3@gmail.com>
2021-11-02 20:53:50 +0000

    Split Parser class into helper classes

---
+++ src/Parsers/Helpers/BodyStructure.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types=1);
+
+namespace Evt\Imap\Parsers\Helpers;
+
+use Evt\Imap\Structures\Body\Structure as BodyStructureStructure; // <- Inspired by Java :/
+use Evt\Imap\Structures\Body\InfoStack as BodyInfoStack;
+use Evt\Imap\Structures\Message\Info as MessageInfo;
+use Evt\Imap\Structures\Attachment as AttachmentInfo;
+
+class BodyStructure
+{
+    public static function parse(string $bodystructure) : BodyStructureStructure
+    {
+        $bodystructure = str_replace("BODYSTRUCTURE (", "", $bodystructure);
+        $nil = "NIL";
+
+        $messageInfoStack = new BodyInfoStack();
+        $attachmentInfoStack = new BodyInfoStack();
+
+        if (strpos($bodystructure, ")(") !== false) {
+            $parts = explode(")(", $bodystructure);
+            foreach ($parts as $part) {
+                $part = trim($part, "(");
+                $bodyInfo = BodyStructurePart::parse($part);
+
+                if ($bodyInfo instanceof MessageInfo) {
+                    $messageInfoStack->push($bodyInfo);
+                } elseif ($bodyInfo instanceof AttachmentInfo) {
+                    $attachmentInfoStack->push($bodyInfo);
+                }
+            }
+        } else {
+            $bodyInfo = BodyStructurePart::parse($bodystructure);
+
+            if ($bodyInfo instanceof MessageInfo) {
+                $messageInfoStack->push($bodyInfo);
+            } else if ($bodyInfo instanceof AttachmentInfo) {
+                $attachmentInfoStack->push($bodyInfo);
+            }
+        }
+
+        return new BodyStructureStructure($messageInfoStack, $attachmentInfoStack);
+    }
+}
---
+++ src/Parsers/Helpers/Envelope.php
@@ -0,0 +1,128 @@
+<?php declare(strict_types=1);
+
+namespace Evt\Imap\Parsers\Helpers;
+
+use Evt\Imap\Structures\Envelope as EnvelopeStructure;
+
+class Envelope
+{
+    /**
+     * Parses a raw envelope returned by a imap server and converts it to a Envelope object
+     * This follows the structure described in rfc3501#section-7.4.2
+     *
+     * @param string $envelope The raw envelope string
+     *
+     * @return Evt\Imap\Structures\Envelope
+     */
+    public static function parse($envelope) : EnvelopeStructure
+    {
+        $nil = "NIL";
+        $rawEnvelope = trim(trim(trim(substr($envelope, strpos($envelope, "ENVELOPE") + strlen("ENVELOPE"), strlen($envelope))), "("), ")");
+
+        // Extract the date and remove it from the envelope
+        $dateStart = strpos($rawEnvelope, "\"");
+        $dateEnd = strpos($rawEnvelope, "\" ") + strlen("\" ");
+        $messyDate = substr($rawEnvelope, $dateStart, $dateEnd);
+        $rawEnvelope = substr_replace($rawEnvelope, "", $dateStart, $dateEnd);
+
+        // Extract and remove the subject
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messySubject = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $subjectStart = strpos($rawEnvelope, "\"");
+            $subjectEnd = strpos($rawEnvelope, "\" ", 2) + strlen("\" ");
+            $messySubject = substr($rawEnvelope, 0, $subjectEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $subjectEnd);
+        }
+
+        // Extract and remove the from address
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyFrom = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $fromStart = strpos($rawEnvelope, "((");
+            $fromEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messyFrom = substr($rawEnvelope, 0, $fromEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $fromEnd);
+        }
+
+        // Extract and remove the sender address
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messySender = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $senderStart = strpos($rawEnvelope, "((");
+            $senderEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messySender = substr($rawEnvelope, 0, $senderEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $senderEnd);
+        }
+
+        // Extract and remove the reply-to address
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyReplyTo = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $replyToStart = strpos($rawEnvelope, "((");
+            $replyToEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messyReplyTo = substr($rawEnvelope, 0, $replyToEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $replyToEnd);
+        }
+
+        // Extract and remove the to address
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyTo = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $toStart = strpos($rawEnvelope, "((");
+            $toEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messyTo = substr($rawEnvelope, 0, $toEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $toEnd);
+        }
+
+        // Extract the cc address(es)
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyCc = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $ccStart = strpos($rawEnvelope, "((");
+            $ccEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messyCc = substr($rawEnvelope, 0, $ccEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $ccEnd);
+        }
+
+        // Extract and remove the bcc address(es)
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyBcc = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $bccStart = strpos($rawEnvelope, "((");
+            $bccEnd = strpos($rawEnvelope, ")) ") + strlen(")) ");
+            $messyBcc = substr($rawEnvelope, 0, $bccEnd);
+        }
+
+        // Extract and remove the in-reply-to field
+        if (strpos($rawEnvelope, $nil) === 0) {
+            $messyInReplyTo = $nil;
+            $rawEnvelope = trim(substr_replace($rawEnvelope, "", 0, strlen($nil)));
+        } else {
+            // $inReplyToStart = strpos($rawEnvelope, "\"");
+            $inReplyToEnd = strpos($rawEnvelope, "\" ") + strlen("\" ");
+            $messyInReplyTo = substr($rawEnvelope, 0, $inReplyToEnd);
+            $rawEnvelope = substr_replace($rawEnvelope, "", 0, $inReplyToEnd);
+        }
+
+        $date = trim(trim($messyDate), "\"");
+        $subject = trim(trim($messySubject), "\"");
+        $from = Addresses::parse($messyFrom);
+        $sender = Addresses::parse($messySender);
+        $replyTo = Addresses::parse($messyReplyTo);
+        $to = Addresses::parse($messyTo);
+        $cc = Addresses::parse($messyCc);
+        $bcc = Addresses::parse($messyBcc);
+        $inReplyTo = trim(trim($messyInReplyTo), "\"");
+        $messageId = trim(trim($rawEnvelope), "\"");
+
+        return new EnvelopeStructure($date, $subject, $from, $sender, $replyTo, $to, $cc, $bcc, $inReplyTo, $messageId);
+    }
+}
