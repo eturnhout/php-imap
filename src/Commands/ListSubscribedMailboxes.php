c9d9969b92671eb103501a7e8d30fddb45f60957
Eelke van Turnhout <8569367+eturnhout@users.noreply.github.com>
2021-12-02 19:08:16 +0100

    Add docblock to ListSubscribedMailboxes

--- src/Commands/ListSubscribedMailboxes.php
+++ src/Commands/ListSubscribedMailboxes.php
@@ -2,6 +2,10 @@
 
 namespace Evt\Imap\Commands;
 
+/**
+ * Get a list of subscribed mailboxes and the hierarchy delimiter
+ * Runs the LSUB command described in rfc3501#section-6.3.9
+ */
 class ListSubscribedMailboxes extends ListMailboxes
 {
     public function getCommand(): string
